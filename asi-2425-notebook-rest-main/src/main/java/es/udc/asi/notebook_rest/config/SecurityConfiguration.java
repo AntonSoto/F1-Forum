357d65a6625d519caeb8ed8b5ccce5d9016c2313
Antonetiiii <114655522+AntonSoto@users.noreply.github.com>
2025-01-18 14:39:35 +0100

    Poder eliminar mi propia cuenta

--- asi-2425-notebook-rest-main/src/main/java/es/udc/asi/notebook_rest/config/SecurityConfiguration.java
+++ asi-2425-notebook-rest-main/src/main/java/es/udc/asi/notebook_rest/config/SecurityConfiguration.java
@@ -62,6 +62,7 @@ public class SecurityConfiguration {
       .authorizeHttpRequests((authorize) -> authorize
         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
         .requestMatchers(HttpMethod.POST, "/api/authenticate").permitAll()
+        .requestMatchers(HttpMethod.DELETE, "/api/users/**").permitAll()
         .requestMatchers(HttpMethod.POST, "/api/register").permitAll()
         .requestMatchers(HttpMethod.GET, "/api/notes/**").authenticated()
         .requestMatchers(HttpMethod.GET, "/api/users/**").hasAnyAuthority(UserAuthority.ADMIN.toString())
